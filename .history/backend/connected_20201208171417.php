56416f02baadba7a4d7ca3f0d6652fa8e5b2da59
Nicolas Faessel <niko@debian-tower>
2020-12-08 17:40:28 +0100

    add .gitignore and rmoved .history

--- .history/backend/connected_20201208171417.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    session_start(); 
-    if(!empty($_SESSION['utilisateur'])) {
-        echo json_encode(["connected" => true, 'login' => $_SESSION['utilisateur']]);
-    } else {
-        echo json_encode(["connected" => false]);
-    }
